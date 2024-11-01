7add2633fced17789cdff4a8f1df385e96f8cff5
harshida ck <harshidack50@gmail.com>
2024-11-01 11:07:10 +0530

    capacitor.config.json file added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wariyum.app',
+  appName: 'capacitor-angular-app',
+  webDir: 'dist'
+};
+
+export default config;
